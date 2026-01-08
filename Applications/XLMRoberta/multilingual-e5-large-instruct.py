2dd658d5f8a01aa10b9991ca522830ff16cd0bc3
junbong.yu <junbong.yu@samsung.com>
2026-01-07 19:55:13 -0500

    XLMRoverta: temporary xlm_roberta v1

--- Applications/XLMRoberta/multilingual-e5-large-instruct.py
+++ Applications/XLMRoberta/multilingual-e5-large-instruct.py
@@ -19,7 +19,7 @@ queries = [
     get_detailed_instruct(task, 'how much protein should a female eat'),
     get_detailed_instruct(task, '南瓜的家常做法')
 ]
-# No need to add instruction for retrieval documents
+# No need to add instruction for retrieval doc1ments
 documents = [
     "As a general guideline, the CDC's average requirement of protein for women ages 19 to 70 is 46 grams per day. But, as you can see from this chart, you'll need to increase that if you're expecting or training for a marathon. Check out the chart below to see how much protein you should be eating each day.",
     "1.清炒南瓜丝 原料:嫩南瓜半个 调料:葱、盐、白糖、鸡精 做法: 1、南瓜用刀薄薄的削去表面一层皮,用勺子刮去瓤 2、擦成细丝(没有擦菜板就用刀慢慢切成细丝) 3、锅烧热放油,入葱花煸出香味 4、入南瓜丝快速翻炒一分钟左右,放盐、一点白糖和鸡精调味出锅 2.香葱炒南瓜 原料:南瓜1只 调料:香葱、蒜末、橄榄油、盐 做法: 1、将南瓜去皮,切成片 2、油锅8成热后,将蒜末放入爆香 3、爆香后,将南瓜片放入,翻炒 4、在翻炒的同时,可以不时地往锅里加水,但不要太多 5、放入盐,炒匀 6、南瓜差不多软和绵了之后,就可以关火 7、撒入香葱,即可出锅"
@@ -29,24 +29,24 @@ input_texts = queries + documents
 tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large-instruct')
 model = AutoModel.from_pretrained('intfloat/multilingual-e5-large-instruct')
 
-print(model)
+# print(model)
 
-for param_tensor in model.state_dict():
-    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+# for param_tensor in model.state_dict():
+#     print(param_tensor, "\t", model.state_dict()[param_tensor].size())
 
-for name, param in model.state_dict().items():
-    print(f"{name}: {param.data_ptr()}")
+# for name, param in model.state_dict().items():
+#     print(f"{name}: {param.data_ptr()}")
 
-# # Tokenize the input texts
-# batch_dict = tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
+# Tokenize the input texts
+batch_dict = tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
 
-# outputs = model(**batch_dict)
-# embeddings = average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+outputs = model(**batch_dict)
+embeddings = average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
 
-# # normalize embeddings
-# embeddings = F.normalize(embeddings, p=2, dim=1)
-# scores = (embeddings[:2] @ embeddings[2:].T) * 100
-# print(scores.tolist())
+# normalize embeddings
+embeddings = F.normalize(embeddings, p=2, dim=1)
+scores = (embeddings[:2] @ embeddings[2:].T) * 100
+print(scores.tolist())
 # => [[91.92852783203125, 67.580322265625], [70.3814468383789, 92.1330795288086]]
 
 # XLMRobertaModel(
