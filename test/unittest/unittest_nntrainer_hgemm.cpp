1f6babd8d911e325b3306ef1c6e10ea23f78a5d9
junbong.yu <junbong.yu@samsung.com>
2026-02-11 08:22:40 +0900

    add hgemm test

--- test/unittest/unittest_nntrainer_hgemm.cpp
+++ test/unittest/unittest_nntrainer_hgemm.cpp
@@ -2,7 +2,7 @@
 /**
  * @file	unittest_nntrainer_hgemm.cpp
  * @date	10 February 2026
- * @brief	This is unittest for hgemm operations
+ * @brief	This is unittest for hgemm operations with pure __fp16
  * @see		https://github.com/nntrainer/nntrainer
  * @author	OpenCode
  * @bug		No known bugs except for NYI items
@@ -10,7 +10,6 @@
 
 #include "nntrainer_test_util.h"
 #include <cpu_backend.h>
-#include <fallback_internal.h>
 #include <fp16.h>
 #include <gtest/gtest.h>
 #include <hgemm.h>
@@ -30,26 +29,30 @@ using std::chrono::seconds;
 template <typename T>
 static inline double find_max_diff(T *src, T *src2, int M, int N) {
   float max_diff = 0;
-  double err_sum = 0;
   for (int i = 0; i < M; ++i) {
     for (int j = 0; j < N; ++j) {
-      max_diff = std::max(max_diff, std::abs(static_cast<float>(
-                                      src[i * N + j] - src2[i * N + j])));
-      err_sum += std::abs(static_cast<float>(src[i * N + j] - src2[i * N + j]));
+      max_diff =
+        std::max(max_diff, std::abs(static_cast<float>(src[i * N + j] -
+                                                        src2[i * N + j])));
     }
   }
   return max_diff;
 }
 
-template <typename T = float>
+template <typename T = __fp16>
 float compute_mse(const uint32_t M, const uint32_t N, std::vector<T> &ref_dst,
                   std::vector<T> &dst, bool print = false) {
   auto mean_squared_error = mse<T, T>(ref_dst.data(), dst.data(), M * N);
   auto cos_sim = cosine_similarity<T, T>(ref_dst.data(), dst.data(), M * N);
   auto max_differ = find_max_diff<T>(ref_dst.data(), dst.data(), M, N);
 
-  auto sum = std::accumulate(dst.begin(), dst.end(), 0.0);
-  auto sum_gt = std::accumulate(ref_dst.begin(), ref_dst.end(), 0.0);
+  float sum = 0;
+  float sum_gt = 0;
+  for (size_t i = 0; i < dst.size(); ++i) {
+    sum += static_cast<float>(dst[i]);
+    sum_gt += static_cast<float>(ref_dst[i]);
+  }
+
   if (print) {
     std::cout << "[INFO]            MSE: " << mean_squared_error
               << ", COS_SIM: " << cos_sim << ", MAX_DIFFER: " << max_differ
@@ -58,31 +61,23 @@ float compute_mse(const uint32_t M, const uint32_t N, std::vector<T> &ref_dst,
   return mean_squared_error;
 }
 
-static std::vector<float> convert_fp16_to_fp32(const std::vector<__fp16> &fp16_vec) {
-  std::vector<float> fp32_vec(fp16_vec.size());
-  for (size_t i = 0; i < fp16_vec.size(); ++i) {
-    fp32_vec[i] = static_cast<float>(fp16_vec[i]);
-  }
-  return fp32_vec;
-}
-
-static std::vector<__fp16> convert_fp32_to_fp16(const std::vector<float> &fp32_vec) {
-  std::vector<__fp16> fp16_vec(fp32_vec.size());
-  for (size_t i = 0; i < fp32_vec.size(); ++i) {
-    fp16_vec[i] = static_cast<__fp16>(fp32_vec[i]);
+static std::vector<__fp16> generate_random_fp16(size_t size) {
+  std::vector<float> f32_vec = generate_random_vector<float>(size);
+  std::vector<__fp16> fp16_vec(size);
+  for (size_t i = 0; i < size; ++i) {
+    fp16_vec[i] = static_cast<__fp16>(f32_vec[i]);
   }
   return fp16_vec;
 }
 
 float test_hgemm(const uint32_t M, const uint32_t K, const uint32_t N,
-                 const __fp16 *weights, const __fp16 *activations,
-                 std::vector<__fp16> &ref_dst_fp16, bool TransA = false,
-                 bool TransB = false, bool print = false) {
-  std::vector<__fp16> dst_fp16(M * N);
-  
+                  const __fp16 *weights, const __fp16 *activations,
+                  std::vector<__fp16> &ref_dst, bool TransA = false,
+                  bool TransB = false, bool print = false) {
+  std::vector<__fp16> dst(M * N);
+
   auto t1 = high_resolution_clock::now();
-  hgemm(activations, weights, dst_fp16.data(), M, N, K, 1.0f, 0.0f, TransA,
-        TransB);
+  hgemm(activations, weights, dst.data(), M, N, K, 1.0f, 0.0f, TransA, TransB);
   auto t2 = high_resolution_clock::now();
   auto dt = duration_cast<nanoseconds>(t2 - t1);
   if (print) {
@@ -91,18 +86,18 @@ float test_hgemm(const uint32_t M, const uint32_t K, const uint32_t N,
               << " ms " << std::endl;
   }
 
-  auto mean_squared_error = compute_mse(M, N, ref_dst_fp16, dst_fp16, print);
+  auto mean_squared_error = compute_mse(M, N, ref_dst, dst, print);
   return mean_squared_error;
 }
 
 float test_hgemm_small(const uint32_t M, const uint32_t K, const uint32_t N,
-                       const __fp16 *weights, const __fp16 *activations,
-                       std::vector<__fp16> &ref_dst_fp16, bool TransA = false,
-                       bool TransB = false, bool print = false) {
-  std::vector<__fp16> dst_fp16(M * N);
-  
+                        const __fp16 *weights, const __fp16 *activations,
+                        std::vector<__fp16> &ref_dst, bool TransA = false,
+                        bool TransB = false, bool print = false) {
+  std::vector<__fp16> dst(M * N);
+
   auto t1 = high_resolution_clock::now();
-  hgemm_small(activations, weights, dst_fp16.data(), M, N, K, 1.0f, 0.0f, TransA,
+  hgemm_small(activations, weights, dst.data(), M, N, K, 1.0f, 0.0f, TransA,
               TransB);
   auto t2 = high_resolution_clock::now();
   auto dt = duration_cast<nanoseconds>(t2 - t1);
@@ -112,19 +107,18 @@ float test_hgemm_small(const uint32_t M, const uint32_t K, const uint32_t N,
               << " ms " << std::endl;
   }
 
-  auto mean_squared_error = compute_mse(M, N, ref_dst_fp16, dst_fp16, print);
+  auto mean_squared_error = compute_mse(M, N, ref_dst, dst, print);
   return mean_squared_error;
 }
 
 float test_hgemm_K1(const uint32_t M, const uint32_t K, const uint32_t N,
-                    const __fp16 *weights, const __fp16 *activations,
-                    std::vector<__fp16> &ref_dst_fp16, bool TransA = false,
-                    bool TransB = false, bool print = false) {
-  std::vector<__fp16> dst_fp16(M * N);
-  
+                     const __fp16 *weights, const __fp16 *activations,
+                     std::vector<__fp16> &ref_dst, bool TransA = false,
+                     bool TransB = false, bool print = false) {
+  std::vector<__fp16> dst(M * N);
+
   auto t1 = high_resolution_clock::now();
-  hgemm_K1(activations, weights, dst_fp16.data(), M, N, K, 1.0f, 0.0f, TransA,
-           TransB);
+  hgemm_K1(activations, weights, dst.data(), M, N, K, 1.0f, 0.0f, TransA, TransB);
   auto t2 = high_resolution_clock::now();
   auto dt = duration_cast<nanoseconds>(t2 - t1);
   if (print) {
@@ -133,14 +127,14 @@ float test_hgemm_K1(const uint32_t M, const uint32_t K, const uint32_t N,
               << " ms " << std::endl;
   }
 
-  auto mean_squared_error = compute_mse(M, N, ref_dst_fp16, dst_fp16, print);
+  auto mean_squared_error = compute_mse(M, N, ref_dst, dst, print);
   return mean_squared_error;
 }
 
 static void run_hgemm_test(const uint32_t M, const uint32_t K, const uint32_t N,
-                           float &hgemm_mse, float &hgemm_small_mse,
-                           bool TransA = false, bool TransB = false,
-                           bool print = false) {
+                            float &hgemm_mse, float &hgemm_small_mse,
+                            bool TransA = false, bool TransB = false,
+                            bool print = false) {
   nntrainer::init_backend();
 
   if (print) {
@@ -149,90 +143,40 @@ static void run_hgemm_test(const uint32_t M, const uint32_t K, const uint32_t N,
               << std::endl;
   }
 
-  std::vector<float> activation_f32 = generate_random_vector<float>(M * K);
-  std::vector<float> weight_f32 = generate_random_vector<float>(N * K);
-  
-  std::vector<__fp16> activation_fp16 = convert_fp32_to_fp16(activation_f32);
-  std::vector<__fp16> weight_fp16 = convert_fp32_to_fp16(weight_f32);
-  
-  std::vector<__fp16> ref_dst_fp16(M * N);
-  std::vector<float> ref_dst_f32(M * N);
+  std::vector<__fp16> activation_fp16 = generate_random_fp16(M * K);
+  std::vector<__fp16> weight_fp16 = generate_random_fp16(N * K);
+  std::vector<__fp16> ref_dst(M * N);
 
   auto t1 = high_resolution_clock::now();
   for (int tc = 0; tc < 10; ++tc) {
-    nntrainer::sgemm(0, TransA, TransB, M, N, K, 1.F, activation_f32.data(), K,
-                     weight_f32.data(), K, 0.F, ref_dst_f32.data(), N);
+    hgemm(activation_fp16.data(), weight_fp16.data(), ref_dst.data(), M, N, K,
+           1.0f, 0.0f, TransA, TransB);
   }
   auto t2 = high_resolution_clock::now();
   auto dt = duration_cast<nanoseconds>(t2 - t1);
   if (print) {
-    std::cout << "[INFO] sgemm (avg): " << dt.count() / 10 << " ns "
-              << dt.count() / 10 / 1'000 << " us " << dt.count() / 10 / 1'000'000
-              << " ms " << std::endl;
+    std::cout << "[INFO] hgemm ref (avg): " << dt.count() / 10 << " ns "
+              << dt.count() / 10 / 1'000 << " us "
+              << dt.count() / 10 / 1'000'000 << " ms " << std::endl;
   }
 
-  ref_dst_fp16 = convert_fp32_to_fp16(ref_dst_f32);
-
-  hgemm_mse = test_hgemm(M, K, N, weight_fp16.data(), activation_fp16.data(),
-                         ref_dst_fp16, TransA, TransB, print);
+  hgemm_mse =
+    test_hgemm(M, K, N, weight_fp16.data(), activation_fp16.data(), ref_dst,
+               TransA, TransB, print);
   hgemm_small_mse =
     test_hgemm_small(M, K, N, weight_fp16.data(), activation_fp16.data(),
-                     ref_dst_fp16, TransA, TransB, print);
+                     ref_dst, TransA, TransB, print);
 }
 
-TEST(nntrainer_hgemm, basic_hgemm_256x256x256) {
-  const unsigned int M = 256;
-  const unsigned int K = 256;
-  const unsigned int N = 256;
+TEST(nntrainer_hgemm, basic_hgemm_1024x3072x3072) {
+  const unsigned int M = 1024;
+  const unsigned int K = 3072;
+  const unsigned int N = 3072;
   float hgemm_mse, hgemm_small_mse;
-  constexpr float eps = 1e-3;
+  constexpr float eps = 1e-5;
   run_hgemm_test(M, K, N, hgemm_mse, hgemm_small_mse, false, false, true);
-  ASSERT_LE(hgemm_mse, eps * M * K * N);
-  ASSERT_LE(hgemm_small_mse, eps * M * K * N);
-}
-
-TEST(nntrainer_hgemm, basic_hgemm_512x512x512) {
-  const unsigned int M = 512;
-  const unsigned int K = 512;
-  const unsigned int N = 512;
-  float hgemm_mse, hgemm_small_mse;
-  constexpr float eps = 1e-3;
-  run_hgemm_test(M, K, N, hgemm_mse, hgemm_small_mse, false, false, true);
-  ASSERT_LE(hgemm_mse, eps * M * K * N);
-  ASSERT_LE(hgemm_small_mse, eps * M * K * N);
-}
-
-TEST(nntrainer_hgemm, basic_hgemm_256x512x512) {
-  const unsigned int M = 256;
-  const unsigned int K = 512;
-  const unsigned int N = 512;
-  float hgemm_mse, hgemm_small_mse;
-  constexpr float eps = 1e-3;
-  run_hgemm_test(M, K, N, hgemm_mse, hgemm_small_mse, false, false, true);
-  ASSERT_LE(hgemm_mse, eps * M * K * N);
-  ASSERT_LE(hgemm_small_mse, eps * M * K * N);
-}
-
-TEST(nntrainer_hgemm, basic_hgemm_512x256x128) {
-  const unsigned int M = 512;
-  const unsigned int K = 256;
-  const unsigned int N = 128;
-  float hgemm_mse, hgemm_small_mse;
-  constexpr float eps = 1e-3;
-  run_hgemm_test(M, K, N, hgemm_mse, hgemm_small_mse, false, false, true);
-  ASSERT_LE(hgemm_mse, eps * M * K * N);
-  ASSERT_LE(hgemm_small_mse, eps * M * K * N);
-}
-
-TEST(nntrainer_hgemm, basic_hgemm_128x256x512) {
-  const unsigned int M = 128;
-  const unsigned int K = 256;
-  const unsigned int N = 512;
-  float hgemm_mse, hgemm_small_mse;
-  constexpr float eps = 1e-3;
-  run_hgemm_test(M, K, N, hgemm_mse, hgemm_small_mse, false, false, true);
-  ASSERT_LE(hgemm_mse, eps * M * K * N);
-  ASSERT_LE(hgemm_small_mse, eps * M * K * N);
+  ASSERT_LE(hgemm_mse, eps);
+  ASSERT_LE(hgemm_small_mse, eps);
 }
 
 static void run_hgemm_transB_test(const uint32_t M, const uint32_t K,
@@ -246,123 +190,58 @@ static void run_hgemm_transB_test(const uint32_t M, const uint32_t K,
               << ", TransB:" << TransB << ")" << std::endl;
   }
 
-  std::vector<float> activation_f32 = generate_random_vector<float>(M * K);
-  std::vector<float> weight_f32 = generate_random_vector<float>(N * K);
-  
-  std::vector<__fp16> activation_fp16 = convert_fp32_to_fp16(activation_f32);
-  std::vector<__fp16> weight_fp16 = convert_fp32_to_fp16(weight_f32);
-  
-  std::vector<__fp16> ref_dst_fp16(M * N);
-  std::vector<float> ref_dst_f32(M * N);
+  std::vector<__fp16> activation_fp16 = generate_random_fp16(M * K);
+  std::vector<__fp16> weight_fp16 = generate_random_fp16(N * K);
+  std::vector<__fp16> ref_dst(M * N);
 
   auto t1 = high_resolution_clock::now();
   for (int tc = 0; tc < 10; ++tc) {
-    nntrainer::sgemm(0, false, TransB, M, N, K, 1.F, activation_f32.data(), K,
-                     weight_f32.data(), K, 0.F, ref_dst_f32.data(), N);
+    hgemm(activation_fp16.data(), weight_fp16.data(), ref_dst.data(), M, N, K,
+           1.0f, 0.0f, false, TransB);
   }
   auto t2 = high_resolution_clock::now();
   auto dt = duration_cast<nanoseconds>(t2 - t1);
   if (print) {
-    std::cout << "[INFO] sgemm (avg): " << dt.count() / 10 << " ns "
-              << dt.count() / 10 / 1'000 << " us " << dt.count() / 10 / 1'000'000
-              << " ms " << std::endl;
+    std::cout << "[INFO] hgemm ref (avg): " << dt.count() / 10 << " ns "
+              << dt.count() / 10 / 1'000 << " us "
+              << dt.count() / 10 / 1'000'000 << " ms " << std::endl;
   }
 
-  ref_dst_fp16 = convert_fp32_to_fp16(ref_dst_f32);
-
-  hgemm_mse = test_hgemm(M, K, N, weight_fp16.data(), activation_fp16.data(),
-                         ref_dst_fp16, false, TransB, print);
+  hgemm_mse =
+    test_hgemm(M, K, N, weight_fp16.data(), activation_fp16.data(), ref_dst,
+               false, TransB, print);
   hgemm_small_mse =
     test_hgemm_small(M, K, N, weight_fp16.data(), activation_fp16.data(),
-                     ref_dst_fp16, false, TransB, print);
+                     ref_dst, false, TransB, print);
 }
 
-TEST(nntrainer_hgemm, hgemm_transB_256x256x256) {
-  const unsigned int M = 256;
-  const unsigned int K = 256;
-  const unsigned int N = 256;
+TEST(nntrainer_hgemm, hgemm_transB_1024x3072x3072) {
+  const unsigned int M = 1024;
+  const unsigned int K = 3072;
+  const unsigned int N = 3072;
   float hgemm_mse, hgemm_small_mse;
-  constexpr float eps = 1e-3;
+  constexpr float eps = 1e-5;
   run_hgemm_transB_test(M, K, N, hgemm_mse, hgemm_small_mse, true, true);
-  ASSERT_LE(hgemm_mse, eps * M * K * N);
-  ASSERT_LE(hgemm_small_mse, eps * M * K * N);
+  ASSERT_LE(hgemm_mse, eps);
+  ASSERT_LE(hgemm_small_mse, eps);
 }
 
-TEST(nntrainer_hgemm, hgemm_transB_512x512x512) {
-  const unsigned int M = 512;
-  const unsigned int K = 512;
-  const unsigned int N = 512;
-  float hgemm_mse, hgemm_small_mse;
-  constexpr float eps = 1e-3;
-  run_hgemm_transB_test(M, K, N, hgemm_mse, hgemm_small_mse, true, true);
-  ASSERT_LE(hgemm_mse, eps * M * K * N);
-  ASSERT_LE(hgemm_small_mse, eps * M * K * N);
-}
+GTEST_API_ int main(int argc, char **argv) {
+  int result = -1;
 
-static void run_hgemm_K1_test(const uint32_t M, const uint32_t K,
-                              const uint32_t N, float &hgemm_K1_mse,
-                              bool print = false) {
-  nntrainer::init_backend();
-
-  if (print) {
-    std::cout << "[INFO] HGEMM K1 Test (M:" << M << ", K:" << K << ", N:" << N
-              << ")" << std::endl;
+  try {
+    testing::InitGoogleTest(&argc, argv);
+  } catch (...) {
+    std::cerr << "Error during InitGoogleTest" << std::endl;
+    return 0;
   }
 
-  std::vector<float> activation_f32 = generate_random_vector<float>(M * K);
-  std::vector<float> weight_f32 = generate_random_vector<float>(N * K);
-  
-  std::vector<__fp16> activation_fp16 = convert_fp32_to_fp16(activation_f32);
-  std::vector<__fp16> weight_fp16 = convert_fp32_to_fp16(weight_f32);
-  
-  std::vector<__fp16> ref_dst_fp16(M * N);
-  std::vector<float> ref_dst_f32(M * N);
-
-  auto t1 = high_resolution_clock::now();
-  for (int tc = 0; tc < 10; ++tc) {
-    nntrainer::sgemm(0, false, false, M, N, K, 1.F, activation_f32.data(), K,
-                     weight_f32.data(), K, 0.F, ref_dst_f32.data(), N);
-  }
-  auto t2 = high_resolution_clock::now();
-  auto dt = duration_cast<nanoseconds>(t2 - t1);
-  if (print) {
-    std::cout << "[INFO] sgemm (avg): " << dt.count() / 10 << " ns "
-              << dt.count() / 10 / 1'000 << " us " << dt.count() / 10 / 1'000'000
-              << " ms " << std::endl;
+  try {
+    result = RUN_ALL_TESTS();
+  } catch (...) {
+    std::cerr << "Error during RUN_ALL_TESTS()" << std::endl;
   }
 
-  ref_dst_fp16 = convert_fp32_to_fp16(ref_dst_f32);
-
-  hgemm_K1_mse = test_hgemm_K1(M, K, N, weight_fp16.data(), activation_fp16.data(),
-                               ref_dst_fp16, false, false, print);
+  return result;
 }
 
-TEST(nntrainer_hgemm, hgemm_K1_256x1x256) {
-  const unsigned int M = 256;
-  const unsigned int K = 1;
-  const unsigned int N = 256;
-  float hgemm_K1_mse;
-  constexpr float eps = 1e-3;
-  run_hgemm_K1_test(M, K, N, hgemm_K1_mse, true);
-  ASSERT_LE(hgemm_K1_mse, eps * M);
-}
-
-TEST(nntrainer_hgemm, hgemm_K1_512x1x512) {
-  const unsigned int M = 512;
-  const unsigned int K = 1;
-  const unsigned int N = 512;
-  float hgemm_K1_mse;
-  constexpr float eps = 1e-3;
-  run_hgemm_K1_test(M, K, N, hgemm_K1_mse, true);
-  ASSERT_LE(hgemm_K1_mse, eps * M);
-}
-
-TEST(nntrainer_hgemm, hgemm_K1_128x1x512) {
-  const unsigned int M = 128;
-  const unsigned int K = 1;
-  const unsigned int N = 512;
-  float hgemm_K1_mse;
-  constexpr float eps = 1e-3;
-  run_hgemm_K1_test(M, K, N, hgemm_K1_mse, true);
-  ASSERT_LE(hgemm_K1_mse, eps * M);
-}
